817c67e34d07e4b4d71f690257e54989ae0f5e8d
Yi Ren <coderquestny@gmail.com>
2016-04-16 21:46:09 -0400

    Added restaurant logo and hero slider.

--- js/instagram.js
+++ js/instagram.js
@@ -1,6 +1,6 @@
 var userFeed = new Instafeed({
         get: 'user',
-        userId: '2289781679',
+        userId: '2503186634',
         clientId: '467ede5a6b9b48ae8e03f4e2582aeeb3',
 		resolution: 'thumbnail',
 		limit: '24',
